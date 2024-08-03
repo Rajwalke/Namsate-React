71681ebba44cc0d73f158b8c0042042f084ad166
RajWalke <rajwalke66@gmail.com>
2024-08-04 02:57:04 +0530

    branch is ready for code

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
 {/* <div class="parent">
 <div class="child">
     <h1>I'm H1 Tag</h1>
 </div>
 </div> */}
 // create using react 
-const parent=React.createEleent("div",{id:"parent"},
+const parent=React.createElement("div",{id:"parent"},
     [
     React.createElement("div",{id:"child"},
         [
-        React.createElement("h1",{},"I'm an h1 Tag"),
+        React.createElement("h1",{},"I'm an h1 Tag "),
         React.createElement("h2",{},"I'm an h2 Tag")
         ]
     ),
